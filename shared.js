6ad4c571656378123d226da07f534bd8277eeb37
Mahfudjtf <90707714+Mahfudjtf@users.noreply.github.com>
2026-07-03 18:11:02 +0700

    Add files via upload

--- shared.js
+++ shared.js
@@ -223,6 +223,8 @@ function normalizeModul(name) {
   if (n.indexOf('MAINTENANCE')>=0 || n.indexOf('REPORT')>=0) return 'MAINTENANCE_REPORT';
   if (n.indexOf('COAL')>=0 || n.indexOf('FEEDER')>=0) return 'COAL_FEEDER';
   if (n.indexOf('DCS')>=0 || n.indexOf('HMI')>=0 || n.indexOf('OIS')>=0) return 'DCS_HMI';
+  if (n.indexOf('FLOW METER')>=0 || n.indexOf('FLOWMETER')>=0 || n.indexOf('FGD')>=0) return 'FLOWMETER_FGD';
+  if (n.indexOf('CONDUCTIVITY')>=0) return 'CONDUCTIVITY';
   if (n.indexOf('PH')>=0 || n.indexOf('TRANSMITTER')>=0 || n.indexOf('AIT')>=0 || n.indexOf('ANALYZER')>=0) return 'PH-ANALYZER';
   return n;
 }
